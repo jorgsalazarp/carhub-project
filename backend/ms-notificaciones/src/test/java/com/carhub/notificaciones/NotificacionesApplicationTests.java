abddeb73ab5fcd60f1deca3781282d2d5ccfae02
jorgsalazarp <jorg.salazarp@duocuc.cl>
2026-06-28 02:39:48 -0400

    creacion application para ms-notificaciones

--- backend/ms-notificaciones/src/test/java/com/carhub/notificaciones/NotificacionesApplicationTests.java
+++ backend/ms-notificaciones/src/test/java/com/carhub/notificaciones/NotificacionesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.carhub.notificaciones.notificaciones;
+package com.carhub.notificaciones;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
